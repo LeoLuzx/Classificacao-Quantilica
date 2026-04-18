69bf25fe25b82b83075e7423a68a274b79b18002
Leandro Luz <leandroluzstat@gmail.com>
2026-04-18 17:23:04 -0300

    Criando novo repositório

---
+++ main.R
@@ -0,0 +1,8 @@
+
+# Carregamento das funções
+# Carregamento da imagem
+# Definição das áreas de treino e teste
+# Rodar o modelo
+# - Definir quantil: percentil, quintil, decil, quartil etc.
+# - Definir o número de cortes da distribuição dos dados da imagem
+# - Processar o grid
